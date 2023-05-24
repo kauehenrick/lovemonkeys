921649866f8d07014e19d5553d79677b83f39f6c
Kauê Henrick <kauek78942@gmail.com>
2023-05-24 11:36:39 -0300

    new features

--- assets/scripts/gallery.js
+++ assets/scripts/gallery.js
@@ -1,45 +1,45 @@
 const galeria = [
     {
-        src: "assets/img/(1).jpg"
+        src: "assets/img/ (1).jpg"
     },
     {
-        src: "assets/img/(2).jpg"
+        src: "assets/img/ (2).jpg"
     },
     {
-        src: "assets/img/(3).jpg"
+        src: "assets/img/ (3).jpg"
     },
     {
-        src: "assets/img/(4).jpg"
+        src: "assets/img/ (4).jpg"
     },
     {
-        src: "assets/img/(5).jpg"
+        src: "assets/img/ (5).jpg"
     },
     {
-        src: "assets/img/(6).jpg"
+        src: "assets/img/ (6).jpg"
     },
     {
-        src: "assets/img/(7).jpg"
+        src: "assets/img/ (7).jpg"
     },
     {
-        src: "assets/img/(8).jpg"
+        src: "assets/img/ (8).jpg"
     },
     {
-        src: "assets/img/(9).jpg"
+        src: "assets/img/ (9).jpg"
     },
     {
-        src: "assets/img/(10).jpg"
+        src: "assets/img/ (10).jpg"
     },
     {
-        src: "assets/img/(11).jpg"
+        src: "assets/img/ (11).jpg"
     },
     {
-        src: "assets/img/(12).jpg"
+        src: "assets/img/ (12).jpg"
     },
     {
-        src: "assets/img/(13).jpg"
+        src: "assets/img/ (13).jpg"
     },
     {
-        src: "assets/img/(14).jpg"
+        src: "assets/img/ (14).jpg"
     },
     {
         src: "assets/img/(15).jpg"
